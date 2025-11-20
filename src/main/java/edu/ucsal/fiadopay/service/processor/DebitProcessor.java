c68648a1da3ad45dcfb7592bce851442afde1a77
Kayke Queiroz <devsouza.exe@gmail.com>
2025-11-20 20:33:46 -0300

    feat(processors): implement basic payment strategies (Pix, Boleto, Debit)

---
+++ src/main/java/edu/ucsal/fiadopay/service/processor/DebitProcessor.java
@@ -0,0 +1,18 @@
+package edu.ucsal.fiadopay.service.processor;
+
+import edu.ucsal.fiadopay.annotation.PaymentMethod;
+import edu.ucsal.fiadopay.domain.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+@PaymentMethod(type = "DEBIT")
+public class DebitProcessor implements PaymentProcessor{
+
+    @Override
+    public Payment process(Payment payment) {
+        payment.setStatus(Payment.Status.PENDING);
+        payment.setTotalWithInterest(payment.getAmount());
+
+        return payment;
+    }
+}
